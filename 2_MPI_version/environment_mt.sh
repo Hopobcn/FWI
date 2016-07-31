15634c4b5873a6a283af2cd5123048140e8a1ec7
Pau Farre <hopobcn@gmail.com>
2016-07-31 16:47:39 +0200

    Changes for running in Minotauro.

---
+++ 2_MPI_version/environment_mt.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+module purge
+module load intel/16.0.2
+module load impi/5.1.3.181
--- 6_MPI+CUDA_version/environment_mt.sh
+++ 6_MPI+CUDA_version/environment_mt.sh
@@ -2,3 +2,8 @@
 module purge
 module load pgi
 module load cuda/7.5
+
+export MPI_HOME=$HOME/APPS/OPENMPI/1.10.2/pgi-16.5
+export PATH=$MPI_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$MPI_HOME/lib:$LD_LIBRARY_PATH
+#
