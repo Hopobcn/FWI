87553689f28ef770523cd7f629a4cfa2a49aeabb
Pau Farré <pau.farre@bsc.es>
2016-10-13 14:20:58 +0200

    Use PGI 16.9 since PGI 16.5 has a bug that prevents usage of structures in device functions. Since openmp/1.10.2_cuda_pgi requires PGI 16.5 its commented until we fix that.

--- scripts/environment_pgi.sh
+++ scripts/environment_pgi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 module purge
-module load pgi/16.5
+module load pgi/16.9
 module load cuda/7.5
-module load openmpi/1.10.2_cuda_pgi
+#module load openmpi/1.10.2_cuda_pgi
 module load cmake/3.6.2
 
 # Make CTEST VERBOSE when some test fails:
