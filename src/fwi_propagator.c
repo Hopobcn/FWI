d4a1f6924c0c03100e28e6e3be86ce2dd75cafc7
Pau Farré <pau.farre@bsc.es>
2016-10-13 15:46:42 +0200

    Prevent openacc runtime errors when parts of an array are present in the device and others in host.

--- src/fwi_propagator.c
+++ src/fwi_propagator.c
@@ -127,12 +127,10 @@ void compute_component_vcell_TL (      real* restrict vptr,
 #if !defined(USE_CUDA)
 
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
     const integer nelems = dim.pitch * dim.xsize * dim.ysize;
 
-    #pragma acc kernels copyin(szptr[start:nelems], sxptr[start:nelems], syptr[start:nelems], rho[start:nelems]) \
-                        copyin(vptr[start:nelems]) \
+    #pragma acc kernels copyin(szptr[:nelems], sxptr[:nelems], syptr[:nelems], rho[:nelems]) \
+                        copyin(vptr[:nelems]) \
                         copyin(dim) \
                         async(phase) wait(H2D)
     #pragma acc loop independent
@@ -199,12 +197,10 @@ void compute_component_vcell_TR (      real* restrict vptr,
 #ifndef USE_CUDA
 
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
+    const integer nelems = dim.pitch * dim.xsize * dim.ysize;
 
-    #pragma acc kernels copyin(szptr[start:nelems], sxptr[start:nelems], syptr[start:nelems], rho[start:nelems]) \
-                        copyin(vptr[start:nelems]) \
+    #pragma acc kernels copyin(szptr[:nelems], sxptr[:nelems], syptr[:nelems], rho[:nelems]) \
+                        copyin(vptr[:nelems]) \
                         copyin(dim) \
                         async(phase) wait(H2D)
     #pragma acc loop independent 
@@ -271,12 +267,10 @@ void compute_component_vcell_BR (      real* restrict vptr,
 #ifndef USE_CUDA
 
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
+    const integer nelems = dim.pitch * dim.xsize * dim.ysize;
 
-    #pragma acc kernels copyin(szptr[start:nelems], sxptr[start:nelems], syptr[start:nelems], rho[start:nelems]) \
-                        copyin(vptr[start:nelems]) \
+    #pragma acc kernels copyin(szptr[:nelems], sxptr[:nelems], syptr[:nelems], rho[:nelems]) \
+                        copyin(vptr[:nelems]) \
                         copyin(dim) \
                         async(phase) wait(H2D)
     #pragma acc loop independent
@@ -343,12 +337,10 @@ void compute_component_vcell_BL (      real* restrict vptr,
 #ifndef USE_CUDA
 
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
+    const integer nelems = dim.pitch * dim.xsize * dim.ysize;
 
-    #pragma acc kernels copyin(szptr[start:nelems], sxptr[start:nelems], syptr[start:nelems], rho[start:nelems]) \
-                        copyin(vptr[start:nelems]) \
+    #pragma acc kernels copyin(szptr[:nelems], sxptr[:nelems], syptr[:nelems], rho[:nelems]) \
+                        copyin(vptr[:nelems]) \
                         copyin(dim) \
                         async(phase) wait(H2D)
     #pragma acc loop independent 
@@ -653,20 +645,18 @@ void compute_component_scell_TR (s_t             s,
 #ifndef USE_CUDA
     
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
+    const integer nelems = dim.pitch * dim.xsize * dim.ysize;
 
-    #pragma acc kernels copyin(sxxptr[start:nelems], syyptr[start:nelems], szzptr[start:nelems], syzptr[start:nelems], sxzptr[start:nelems], sxyptr[start:nelems]) \
-                        copyin(vxu[start:nelems], vxv[start:nelems], vxw[start:nelems])  \
-                        copyin(vyu[start:nelems], vyv[start:nelems], vyw[start:nelems])  \
-                        copyin(vzu[start:nelems], vzv[start:nelems], vzw[start:nelems])  \
-                        present(cc11[start:nelems], cc12[start:nelems], cc13[start:nelems], cc14[start:nelems], cc15[start:nelems], cc16[start:nelems])             \
-                        present(cc22[start:nelems], cc23[start:nelems], cc24[start:nelems], cc25[start:nelems], cc26[start:nelems])                   \
-                        present(cc33[start:nelems], cc34[start:nelems], cc35[start:nelems], cc36[start:nelems])                         \
-                        present(cc44[start:nelems], cc45[start:nelems], cc46[start:nelems])                               \
-                        present(cc55[start:nelems], cc56[start:nelems])                                     \
-                        present(cc66[start:nelems])                                           \
+    #pragma acc kernels copyin(sxxptr[:nelems], syyptr[:nelems], szzptr[:nelems], syzptr[:nelems], sxzptr[:nelems], sxyptr[:nelems]) \
+                        copyin(vxu[:nelems], vxv[:nelems], vxw[:nelems])  \
+                        copyin(vyu[:nelems], vyv[:nelems], vyw[:nelems])  \
+                        copyin(vzu[:nelems], vzv[:nelems], vzw[:nelems])  \
+                        present(cc11[:nelems], cc12[:nelems], cc13[:nelems], cc14[:nelems], cc15[:nelems], cc16[:nelems])             \
+                        present(cc22[:nelems], cc23[:nelems], cc24[:nelems], cc25[:nelems], cc26[:nelems])                   \
+                        present(cc33[:nelems], cc34[:nelems], cc35[:nelems], cc36[:nelems])                         \
+                        present(cc44[:nelems], cc45[:nelems], cc46[:nelems])                               \
+                        present(cc55[:nelems], cc56[:nelems])                                     \
+                        present(cc66[:nelems])                                           \
                         copyin(dim) \
                         async(phase) 
     #pragma acc loop independent
@@ -813,20 +803,18 @@ void compute_component_scell_TL (s_t             s,
 #ifndef USE_CUDA
     
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
+    const integer nelems = dim.pitch * dim.xsize * dim.ysize;
 
-    #pragma acc kernels copyin(sxxptr[start:nelems], syyptr[start:nelems], szzptr[start:nelems], syzptr[start:nelems], sxzptr[start:nelems], sxyptr[start:nelems]) \
-                        copyin(vxu[start:nelems], vxv[start:nelems], vxw[start:nelems])  \
-                        copyin(vyu[start:nelems], vyv[start:nelems], vyw[start:nelems])  \
-                        copyin(vzu[start:nelems], vzv[start:nelems], vzw[start:nelems])  \
-                        present(cc11[start:nelems], cc12[start:nelems], cc13[start:nelems], cc14[start:nelems], cc15[start:nelems], cc16[start:nelems])             \
-                        present(cc22[start:nelems], cc23[start:nelems], cc24[start:nelems], cc25[start:nelems], cc26[start:nelems])                   \
-                        present(cc33[start:nelems], cc34[start:nelems], cc35[start:nelems], cc36[start:nelems])                         \
-                        present(cc44[start:nelems], cc45[start:nelems], cc46[start:nelems])                               \
-                        present(cc55[start:nelems], cc56[start:nelems])                                     \
-                        present(cc66[start:nelems])                                           \
+    #pragma acc kernels copyin(sxxptr[:nelems], syyptr[:nelems], szzptr[:nelems], syzptr[:nelems], sxzptr[:nelems], sxyptr[:nelems]) \
+                        copyin(vxu[:nelems], vxv[:nelems], vxw[:nelems])  \
+                        copyin(vyu[:nelems], vyv[:nelems], vyw[:nelems])  \
+                        copyin(vzu[:nelems], vzv[:nelems], vzw[:nelems])  \
+                        present(cc11[:nelems], cc12[:nelems], cc13[:nelems], cc14[:nelems], cc15[:nelems], cc16[:nelems])             \
+                        present(cc22[:nelems], cc23[:nelems], cc24[:nelems], cc25[:nelems], cc26[:nelems])                   \
+                        present(cc33[:nelems], cc34[:nelems], cc35[:nelems], cc36[:nelems])                         \
+                        present(cc44[:nelems], cc45[:nelems], cc46[:nelems])                               \
+                        present(cc55[:nelems], cc56[:nelems])                                     \
+                        present(cc66[:nelems])                                           \
                         copyin(dim) \
                         async(phase) 
     #pragma acc loop independent
@@ -974,20 +962,18 @@ void compute_component_scell_BR (s_t             s,
 #ifndef USE_CUDA
     
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
+    const integer nelems = dim.pitch * dim.xsize * dim.ysize;
 
-    #pragma acc kernels copyin(sxxptr[start:nelems], syyptr[start:nelems], szzptr[start:nelems], syzptr[start:nelems], sxzptr[start:nelems], sxyptr[start:nelems]) \
-                        copyin(vxu[start:nelems], vxv[start:nelems], vxw[start:nelems])  \
-                        copyin(vyu[start:nelems], vyv[start:nelems], vyw[start:nelems])  \
-                        copyin(vzu[start:nelems], vzv[start:nelems], vzw[start:nelems])  \
-                        present(cc11[start:nelems], cc12[start:nelems], cc13[start:nelems], cc14[start:nelems], cc15[start:nelems], cc16[start:nelems])             \
-                        present(cc22[start:nelems], cc23[start:nelems], cc24[start:nelems], cc25[start:nelems], cc26[start:nelems])                   \
-                        present(cc33[start:nelems], cc34[start:nelems], cc35[start:nelems], cc36[start:nelems])                         \
-                        present(cc44[start:nelems], cc45[start:nelems], cc46[start:nelems])                               \
-                        present(cc55[start:nelems], cc56[start:nelems])                                     \
-                        present(cc66[start:nelems])                                           \
+    #pragma acc kernels copyin(sxxptr[:nelems], syyptr[:nelems], szzptr[:nelems], syzptr[:nelems], sxzptr[:nelems], sxyptr[:nelems]) \
+                        copyin(vxu[:nelems], vxv[:nelems], vxw[:nelems])  \
+                        copyin(vyu[:nelems], vyv[:nelems], vyw[:nelems])  \
+                        copyin(vzu[:nelems], vzv[:nelems], vzw[:nelems])  \
+                        present(cc11[:nelems], cc12[:nelems], cc13[:nelems], cc14[:nelems], cc15[:nelems], cc16[:nelems])             \
+                        present(cc22[:nelems], cc23[:nelems], cc24[:nelems], cc25[:nelems], cc26[:nelems])                   \
+                        present(cc33[:nelems], cc34[:nelems], cc35[:nelems], cc36[:nelems])                         \
+                        present(cc44[:nelems], cc45[:nelems], cc46[:nelems])                               \
+                        present(cc55[:nelems], cc56[:nelems])                                     \
+                        present(cc66[:nelems])                                           \
                         copyin(dim) \
                         async(phase) 
     #pragma acc loop independent
@@ -1135,20 +1121,18 @@ void compute_component_scell_BL (s_t             s,
 #ifndef USE_CUDA
 
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
+    const integer nelems = dim.pitch * dim.xsize * dim.ysize;
 
-    #pragma acc kernels copyin(sxxptr[start:nelems], syyptr[start:nelems], szzptr[start:nelems], syzptr[start:nelems], sxzptr[start:nelems], sxyptr[start:nelems]) \
-                        copyin(vxu[start:nelems], vxv[start:nelems], vxw[start:nelems])  \
-                        copyin(vyu[start:nelems], vyv[start:nelems], vyw[start:nelems])  \
-                        copyin(vzu[start:nelems], vzv[start:nelems], vzw[start:nelems])  \
-                        present(cc11[start:nelems], cc12[start:nelems], cc13[start:nelems], cc14[start:nelems], cc15[start:nelems], cc16[start:nelems])             \
-                        present(cc22[start:nelems], cc23[start:nelems], cc24[start:nelems], cc25[start:nelems], cc26[start:nelems])                   \
-                        present(cc33[start:nelems], cc34[start:nelems], cc35[start:nelems], cc36[start:nelems])                         \
-                        present(cc44[start:nelems], cc45[start:nelems], cc46[start:nelems])                               \
-                        present(cc55[start:nelems], cc56[start:nelems])                                     \
-                        present(cc66[start:nelems])                                           \
+    #pragma acc kernels copyin(sxxptr[:nelems], syyptr[:nelems], szzptr[:nelems], syzptr[:nelems], sxzptr[:nelems], sxyptr[:nelems]) \
+                        copyin(vxu[:nelems], vxv[:nelems], vxw[:nelems])  \
+                        copyin(vyu[:nelems], vyv[:nelems], vyw[:nelems])  \
+                        copyin(vzu[:nelems], vzv[:nelems], vzw[:nelems])  \
+                        present(cc11[:nelems], cc12[:nelems], cc13[:nelems], cc14[:nelems], cc15[:nelems], cc16[:nelems])             \
+                        present(cc22[:nelems], cc23[:nelems], cc24[:nelems], cc25[:nelems], cc26[:nelems])                   \
+                        present(cc33[:nelems], cc34[:nelems], cc35[:nelems], cc36[:nelems])                         \
+                        present(cc44[:nelems], cc45[:nelems], cc46[:nelems])                               \
+                        present(cc55[:nelems], cc56[:nelems])                                     \
+                        present(cc66[:nelems])                                           \
                         copyin(dim) \
                         async(phase) 
     #pragma acc loop independent
