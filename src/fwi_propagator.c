b6d6e7a477899974218d8c6f88c3211c6b512cb5
Pau Farré <pau.farre@bsc.es>
2017-02-27 18:17:45 +0100

    Remove OpenACC+CUDA glue code.

--- src/fwi_propagator.c
+++ src/fwi_propagator.c
@@ -162,8 +162,6 @@ void compute_component_vcell_TL (      real* restrict vptr,
                                  const integer        dimmx,
                                  const phase_t        phase)
 {
-#if !defined(USE_CUDA)
-
 #if defined(_OPENACC)
     const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
     const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
@@ -200,16 +198,6 @@ void compute_component_vcell_TL (      real* restrict vptr,
             }
         }
     }
-#else /* CUDA KERNELS ENABLED */
-    void* stream = acc_get_cuda_stream(phase);
-
-    #pragma acc host_data use_device(szptr, sxptr, syptr, rho, vptr)
-    {
-        compute_component_vcell_TL_cuda(vptr, szptr, sxptr, syptr, rho,
-                dt, dzi, dxi, dyi, nz0, nzf, nx0, nxf, ny0, nyf, 
-                _SZ, _SX, _SY, dimmz, dimmx, stream);
-    }
-#endif /* end USE_CUDA */
 };
 
 void compute_component_vcell_TR (      real* restrict vptr,
@@ -234,8 +222,6 @@ void compute_component_vcell_TR (      real* restrict vptr,
                                  const integer        dimmx,
                                  const phase_t        phase)
 {
-#ifndef USE_CUDA
-
 #if defined(_OPENACC)
     const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
     const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
@@ -272,16 +258,6 @@ void compute_component_vcell_TR (      real* restrict vptr,
             }
         }
     }
-#else
-    void* stream = acc_get_cuda_stream(phase);
-
-    #pragma acc host_data use_device(szptr, sxptr, syptr, rho, vptr)
-    {
-        compute_component_vcell_TR_cuda(vptr, szptr, sxptr, syptr, rho,
-                dt, dzi, dxi, dyi, nz0, nzf, nx0, nxf, ny0, nyf, 
-                _SZ, _SX, _SY, dimmz, dimmx, stream);
-    }
-#endif
 };
 
 void compute_component_vcell_BR (      real* restrict vptr,
@@ -306,8 +282,6 @@ void compute_component_vcell_BR (      real* restrict vptr,
                                  const integer        dimmx,
                                  const phase_t        phase)
 {
-#ifndef USE_CUDA
-
 #if defined(_OPENACC)
     const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
     const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
@@ -344,16 +318,6 @@ void compute_component_vcell_BR (      real* restrict vptr,
             }
         }
     }
-#else
-    void* stream = acc_get_cuda_stream(phase);
-
-    #pragma acc host_data use_device(szptr, sxptr, syptr, rho, vptr)
-    {
-        compute_component_vcell_BR_cuda(vptr, szptr, sxptr, syptr, rho,
-                dt, dzi, dxi, dyi, nz0, nzf, nx0, nxf, ny0, nyf, 
-                _SZ, _SX, _SY, dimmz, dimmx, stream);
-    }
-#endif
 };
 
 void compute_component_vcell_BL (      real* restrict vptr,
@@ -378,8 +342,6 @@ void compute_component_vcell_BL (      real* restrict vptr,
                                  const integer        dimmx,
                                  const phase_t        phase)
 {
-#ifndef USE_CUDA
-
 #if defined(_OPENACC)
     const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
     const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
@@ -416,16 +378,6 @@ void compute_component_vcell_BL (      real* restrict vptr,
             }
         }
     }
-#else
-    void* stream = acc_get_cuda_stream(phase);
-
-    #pragma acc host_data use_device(szptr, sxptr, syptr, rho, vptr)
-    {
-        compute_component_vcell_BL_cuda(vptr, szptr, sxptr, syptr, rho,
-                dt, dzi, dxi, dyi, nz0, nzf, nx0, nxf, ny0, nyf, 
-                _SZ, _SX, _SY, dimmz, dimmx, stream);
-    }
-#endif
 };
 
 void velocity_propagator(v_t           v,
@@ -699,8 +651,6 @@ void compute_component_scell_TR (s_t             s,
     const real* restrict cc56 = coeffs.c56;
     const real* restrict cc66 = coeffs.c66;
 
-#ifndef USE_CUDA
-    
 #if defined(_OPENACC)
     const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
     const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
@@ -778,24 +728,6 @@ void compute_component_scell_TR (s_t             s,
             }
         }
     }
-#else
-    void* stream = acc_get_cuda_stream(phase);
-
-    #pragma acc host_data use_device(sxxptr, syyptr, szzptr, syzptr, sxzptr, sxyptr, vxu, vxv, vxw, vyu, vyv, vyw, vzu, vzv, vzw, cc11, cc12, cc13, cc14, cc15, cc16, cc22, cc23, cc24, cc25, cc26, cc33, cc34, cc35, cc36, cc44, cc45, cc46, cc55, cc56, cc66) 
-    {
-        compute_component_scell_TR_cuda(
-            sxxptr, syyptr, szzptr, syzptr, sxzptr, sxyptr,
-            vxu, vxv, vxw, vyu, vyv, vyw, vzu, vzv, vzw,
-            cc11, cc12, cc13, cc14, cc15, cc16,
-            cc22, cc23, cc24, cc25, cc26,
-            cc33, cc34, cc35, cc36,
-            cc44, cc45, cc46,
-            cc55, cc56,
-            cc66,
-            dt, dzi, dxi, dyi, nz0, nzf, nx0, nxf, ny0, nyf, _SZ, _SX, _SY, dimmz, dimmx,
-            stream);
-    }
-#endif
 };
 
 void compute_component_scell_TL (s_t             s,
@@ -858,8 +790,6 @@ void compute_component_scell_TL (s_t             s,
     const real* restrict cc55 = coeffs.c55;
     const real* restrict cc56 = coeffs.c56;
     const real* restrict cc66 = coeffs.c66;
-    
-#ifndef USE_CUDA
     
 #if defined(_OPENACC)
     const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
@@ -938,24 +868,6 @@ void compute_component_scell_TL (s_t             s,
             }
         }
     }
-#else
-    void* stream = acc_get_cuda_stream(phase);
-
-    #pragma acc host_data use_device(sxxptr, syyptr, szzptr, syzptr, sxzptr, sxyptr, vxu, vxv, vxw, vyu, vyv, vyw, vzu, vzv, vzw, cc11, cc12, cc13, cc14, cc15, cc16, cc22, cc23, cc24, cc25, cc26, cc33, cc34, cc35, cc36, cc44, cc45, cc46, cc55, cc56, cc66) 
-    {
-        compute_component_scell_TL_cuda(
-            sxxptr, syyptr, szzptr, syzptr, sxzptr, sxyptr,
-            vxu, vxv, vxw, vyu, vyv, vyw, vzu, vzv, vzw,
-            cc11, cc12, cc13, cc14, cc15, cc16,
-            cc22, cc23, cc24, cc25, cc26,
-            cc33, cc34, cc35, cc36,
-            cc44, cc45, cc46,
-            cc55, cc56,
-            cc66,
-            dt, dzi, dxi, dyi, nz0, nzf, nx0, nxf, ny0, nyf, _SZ, _SX, _SY, dimmz, dimmx,
-            stream);
-    }
-#endif
 };
 
 
@@ -1020,8 +932,6 @@ void compute_component_scell_BR (s_t             s,
     const real* restrict cc56 = coeffs.c56;
     const real* restrict cc66 = coeffs.c66;
 
-#ifndef USE_CUDA
-    
 #if defined(_OPENACC)
     const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
     const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
@@ -1100,24 +1010,6 @@ void compute_component_scell_BR (s_t             s,
             }
         }
     }
-#else
-    void* stream = acc_get_cuda_stream(phase);
-
-    #pragma acc host_data use_device(sxxptr, syyptr, szzptr, syzptr, sxzptr, sxyptr, vxu, vxv, vxw, vyu, vyv, vyw, vzu, vzv, vzw, cc11, cc12, cc13, cc14, cc15, cc16, cc22, cc23, cc24, cc25, cc26, cc33, cc34, cc35, cc36, cc44, cc45, cc46, cc55, cc56, cc66) 
-    {
-        compute_component_scell_BR_cuda(
-            sxxptr, syyptr, szzptr, syzptr, sxzptr, sxyptr,
-            vxu, vxv, vxw, vyu, vyv, vyw, vzu, vzv, vzw,
-            cc11, cc12, cc13, cc14, cc15, cc16,
-            cc22, cc23, cc24, cc25, cc26,
-            cc33, cc34, cc35, cc36,
-            cc44, cc45, cc46,
-            cc55, cc56,
-            cc66,
-            dt, dzi, dxi, dyi, nz0, nzf, nx0, nxf, ny0, nyf, _SZ, _SX, _SY, dimmz, dimmx,
-            stream);
-    }
-#endif
 };
 
 void compute_component_scell_BL (s_t             s,
@@ -1181,8 +1073,6 @@ void compute_component_scell_BL (s_t             s,
     const real* restrict cc56 = coeffs.c56;
     const real* restrict cc66 = coeffs.c66;
     
-#ifndef USE_CUDA
-
 #if defined(_OPENACC)
     const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
     const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
@@ -1260,22 +1150,4 @@ void compute_component_scell_BL (s_t             s,
             }
         }
     }
-#else
-    void* stream = acc_get_cuda_stream(phase);
-
-    #pragma acc host_data use_device(sxxptr, syyptr, szzptr, syzptr, sxzptr, sxyptr, vxu, vxv, vxw, vyu, vyv, vyw, vzu, vzv, vzw, cc11, cc12, cc13, cc14, cc15, cc16, cc22, cc23, cc24, cc25, cc26, cc33, cc34, cc35, cc36, cc44, cc45, cc46, cc55, cc56, cc66) 
-    {
-        compute_component_scell_BL_cuda(
-            sxxptr, syyptr, szzptr, syzptr, sxzptr, sxyptr,
-            vxu, vxv, vxw, vyu, vyv, vyw, vzu, vzv, vzw,
-            cc11, cc12, cc13, cc14, cc15, cc16,
-            cc22, cc23, cc24, cc25, cc26,
-            cc33, cc34, cc35, cc36,
-            cc44, cc45, cc46,
-            cc55, cc56,
-            cc66,
-            dt, dzi, dxi, dyi, nz0, nzf, nx0, nxf, ny0, nyf, _SZ, _SX, _SY, dimmz, dimmx,
-            stream);
-    }
-#endif
 };
