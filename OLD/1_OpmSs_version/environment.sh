6e76c7989199c751006b7245d753b0ae4d39acdb
Pau Farré <pau.farre@bsc.es>
2017-02-27 13:14:30 +0100

    Remove legacy code

--- OLD/1_OpmSs_version/environment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-module purge
-module load intel/16.0.0
-module load impi
-module load ompss
-module load extrae
--- OLD/4_OpenACC_version/environment_hulk.sh
+++
@@ -1,9 +0,0 @@
-# Load with cmd: source <this-file-name>
-
-export MPI_HOME=/opt/pgi/linux86-64/2016/mpi/openmpi-1.10.2
-export CUDA_HOME=/usr/local/cuda
-export ACC_HOME=/opt/pgi/linux86-64/16.5
-export PATH=$ACC_HOME/bin:$MPI_HOME/bin:$CUDA_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$ACC_HOME/lib:$MPI_HOME/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH
-# For OpenACC runtime prfiling with github.com/Hopobcn/openacc-nvtx library
-#export ACC_PROFLIB=/home/users/pfarre/scratch-local/CASE/openacc-nvtx/liboaccnvtx.so
