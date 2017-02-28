69ecf77b455816048bc9ffc92673cd66de1cbdca
Pau Farré <pau.farre@bsc.es>
2017-02-28 09:48:39 +0100

    Remove enter/exit data directives + asyncronous kernels + threadblock/warp sizes hints

--- src/fwi_propagator.c
+++ src/fwi_propagator.c
@@ -163,13 +163,7 @@ void compute_component_vcell_TL (      real* restrict vptr,
                                  const phase_t        phase)
 {
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
-
-    #pragma acc kernels copyin(szptr[start:nelems], sxptr[start:nelems], syptr[start:nelems], rho[start:nelems]) \
-                        copyin(vptr[start:nelems]) \
-                        async(phase) wait(H2D)
+    #pragma acc kernels 
     #pragma acc loop independent
 #elif defined(_OPENMP)
     #pragma omp parallel for
@@ -177,12 +171,12 @@ void compute_component_vcell_TL (      real* restrict vptr,
     for(integer y=ny0; y < nyf; y++)
     {
 #if defined(_OPENACC)
-        #pragma acc loop independent device_type(nvidia) gang worker(4)
+        #pragma acc loop independent
 #endif
         for(integer x=nx0; x < nxf; x++)
         {
 #if defined(_OPENACC)
-            #pragma acc loop independent device_type(nvidia) gang vector(32)
+            #pragma acc loop independent
 #elif defined(__INTEL_COMPILER)
             #pragma simd
 #endif
@@ -223,13 +217,7 @@ void compute_component_vcell_TR (      real* restrict vptr,
                                  const phase_t        phase)
 {
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
-
-    #pragma acc kernels copyin(szptr[start:nelems], sxptr[start:nelems], syptr[start:nelems], rho[start:nelems]) \
-                        copyin(vptr[start:nelems]) \
-                        async(phase) wait(H2D)
+    #pragma acc kernels
     #pragma acc loop independent 
 #elif defined(_OPENMP)
     #pragma omp parallel for
@@ -237,12 +225,12 @@ void compute_component_vcell_TR (      real* restrict vptr,
     for(integer y=ny0; y < nyf; y++)
     {
 #if defined(_OPENACC)
-        #pragma acc loop independent device_type(nvidia) gang worker(4)
+        #pragma acc loop independent
 #endif
         for(integer x=nx0; x < nxf; x++)
         {
 #if defined(_OPENACC)
-            #pragma acc loop independent device_type(nvidia) gang vector(32)
+            #pragma acc loop independent
 #elif defined(__INTEL_COMPILER)
             #pragma simd
 #endif
@@ -283,13 +271,7 @@ void compute_component_vcell_BR (      real* restrict vptr,
                                  const phase_t        phase)
 {
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
-
-    #pragma acc kernels copyin(szptr[start:nelems], sxptr[start:nelems], syptr[start:nelems], rho[start:nelems]) \
-                        copyin(vptr[start:nelems]) \
-                        async(phase) wait(H2D)
+    #pragma acc kernels
     #pragma acc loop independent
 #elif defined(_OPENMP)
     #pragma omp parallel for
@@ -297,12 +279,12 @@ void compute_component_vcell_BR (      real* restrict vptr,
     for(integer y=ny0; y < nyf; y++)
     {
 #if defined(_OPENACC)
-        #pragma acc loop independent device_type(nvidia) gang worker(4)
+        #pragma acc loop independent
 #endif
         for(integer x=nx0; x < nxf; x++)
         {
 #if defined(_OPENACC)
-            #pragma acc loop independent device_type(nvidia) gang vector(32)
+            #pragma acc loop independent
 #elif defined(__INTEL_COMPILER)
             #pragma simd
 #endif
@@ -343,13 +325,7 @@ void compute_component_vcell_BL (      real* restrict vptr,
                                  const phase_t        phase)
 {
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
-
-    #pragma acc kernels copyin(szptr[start:nelems], sxptr[start:nelems], syptr[start:nelems], rho[start:nelems]) \
-                        copyin(vptr[start:nelems]) \
-                        async(phase) wait(H2D)
+    #pragma acc kernels
     #pragma acc loop independent 
 #elif defined(_OPENMP)
     #pragma omp parallel for
@@ -357,12 +333,12 @@ void compute_component_vcell_BL (      real* restrict vptr,
     for(integer y=ny0; y < nyf; y++)
     {
 #if defined(_OPENACC)
-        #pragma acc loop independent device_type(nvidia) gang worker(4)
+        #pragma acc loop independent
 #endif
         for(integer x=nx0; x < nxf; x++)
         {
 #if defined(_OPENACC)
-            #pragma acc loop independent device_type(nvidia) gang vector(32)
+            #pragma acc loop independent
 #elif defined(__INTEL_COMPILER)
             #pragma simd
 #endif
@@ -652,21 +628,7 @@ void compute_component_scell_TR (s_t             s,
     const real* restrict cc66 = coeffs.c66;
 
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
-
-    #pragma acc kernels copyin(sxxptr[start:nelems], syyptr[start:nelems], szzptr[start:nelems], syzptr[start:nelems], sxzptr[start:nelems], sxyptr[start:nelems]) \
-                        copyin(vxu[start:nelems], vxv[start:nelems], vxw[start:nelems])  \
-                        copyin(vyu[start:nelems], vyv[start:nelems], vyw[start:nelems])  \
-                        copyin(vzu[start:nelems], vzv[start:nelems], vzw[start:nelems])  \
-                        present(cc11[start:nelems], cc12[start:nelems], cc13[start:nelems], cc14[start:nelems], cc15[start:nelems], cc16[start:nelems])             \
-                        present(cc22[start:nelems], cc23[start:nelems], cc24[start:nelems], cc25[start:nelems], cc26[start:nelems])                   \
-                        present(cc33[start:nelems], cc34[start:nelems], cc35[start:nelems], cc36[start:nelems])                         \
-                        present(cc44[start:nelems], cc45[start:nelems], cc46[start:nelems])                               \
-                        present(cc55[start:nelems], cc56[start:nelems])                                     \
-                        present(cc66[start:nelems])                                           \
-                        async(phase) 
+    #pragma acc kernels
     #pragma acc loop independent
 #elif defined(_OPENMP)
     #pragma omp parallel for
@@ -674,12 +636,12 @@ void compute_component_scell_TR (s_t             s,
     for (integer y = ny0; y < nyf; y++)
     {
 #if defined(_OPENACC)
-        #pragma acc loop independent device_type(nvidia) gang worker(4)
+        #pragma acc loop independent
 #endif
         for (integer x = nx0; x < nxf; x++)
         {
 #if defined(_OPENACC)
-            #pragma acc loop independent device_type(nvidia) gang vector(32)
+            #pragma acc loop independent
 #elif defined(__INTEL_COMPILER)
             #pragma simd
 #endif
@@ -792,21 +754,7 @@ void compute_component_scell_TL (s_t             s,
     const real* restrict cc66 = coeffs.c66;
     
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
-
-    #pragma acc kernels copyin(sxxptr[start:nelems], syyptr[start:nelems], szzptr[start:nelems], syzptr[start:nelems], sxzptr[start:nelems], sxyptr[start:nelems]) \
-                        copyin(vxu[start:nelems], vxv[start:nelems], vxw[start:nelems])  \
-                        copyin(vyu[start:nelems], vyv[start:nelems], vyw[start:nelems])  \
-                        copyin(vzu[start:nelems], vzv[start:nelems], vzw[start:nelems])  \
-                        present(cc11[start:nelems], cc12[start:nelems], cc13[start:nelems], cc14[start:nelems], cc15[start:nelems], cc16[start:nelems])             \
-                        present(cc22[start:nelems], cc23[start:nelems], cc24[start:nelems], cc25[start:nelems], cc26[start:nelems])                   \
-                        present(cc33[start:nelems], cc34[start:nelems], cc35[start:nelems], cc36[start:nelems])                         \
-                        present(cc44[start:nelems], cc45[start:nelems], cc46[start:nelems])                               \
-                        present(cc55[start:nelems], cc56[start:nelems])                                     \
-                        present(cc66[start:nelems])                                           \
-                        async(phase) 
+    #pragma acc kernels
     #pragma acc loop independent
 #elif defined(_OPENMP)
     #pragma omp parallel for
@@ -814,12 +762,12 @@ void compute_component_scell_TL (s_t             s,
     for (integer y = ny0; y < nyf; y++)
     {
 #if defined(_OPENACC)
-        #pragma acc loop independent device_type(nvidia) gang worker(4)
+        #pragma acc loop independent
 #endif
         for (integer x = nx0; x < nxf; x++)
         {
 #if defined(_OPENACC)
-            #pragma acc loop independent device_type(nvidia) gang vector(32) 
+            #pragma acc loop independent
 #elif defined(__INTEL__COMPILER)
             #pragma simd
 #endif
@@ -933,21 +881,7 @@ void compute_component_scell_BR (s_t             s,
     const real* restrict cc66 = coeffs.c66;
 
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
-
-    #pragma acc kernels copyin(sxxptr[start:nelems], syyptr[start:nelems], szzptr[start:nelems], syzptr[start:nelems], sxzptr[start:nelems], sxyptr[start:nelems]) \
-                        copyin(vxu[start:nelems], vxv[start:nelems], vxw[start:nelems])  \
-                        copyin(vyu[start:nelems], vyv[start:nelems], vyw[start:nelems])  \
-                        copyin(vzu[start:nelems], vzv[start:nelems], vzw[start:nelems])  \
-                        present(cc11[start:nelems], cc12[start:nelems], cc13[start:nelems], cc14[start:nelems], cc15[start:nelems], cc16[start:nelems])             \
-                        present(cc22[start:nelems], cc23[start:nelems], cc24[start:nelems], cc25[start:nelems], cc26[start:nelems])                   \
-                        present(cc33[start:nelems], cc34[start:nelems], cc35[start:nelems], cc36[start:nelems])                         \
-                        present(cc44[start:nelems], cc45[start:nelems], cc46[start:nelems])                               \
-                        present(cc55[start:nelems], cc56[start:nelems])                                     \
-                        present(cc66[start:nelems])                                           \
-                        async(phase) 
+    #pragma acc kernels
     #pragma acc loop independent
 #elif defined(_OPENMP)
     #pragma omp parallel for
@@ -955,12 +889,12 @@ void compute_component_scell_BR (s_t             s,
     for (integer y = ny0; y < nyf; y++)
     {
 #if defined(_OPENACC)
-        #pragma acc loop independent device_type(nvidia) gang worker(4)
+        #pragma acc loop independent
 #endif
         for (integer x = nx0; x < nxf; x++)
         {
 #if defined(_OPENACC)
-            #pragma acc loop independent device_type(nvidia) gang vector(32) 
+            #pragma acc loop independent
 #elif defined(__INTEL__COMPILER)
             #pragma simd
 #endif
@@ -1074,21 +1008,7 @@ void compute_component_scell_BL (s_t             s,
     const real* restrict cc66 = coeffs.c66;
     
 #if defined(_OPENACC)
-    const integer start  = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (ny0 - HALO);
-    const integer end    = ((nzf-nz0) + 2*HALO) * ((nxf-nx0) + 2*HALO) * (nyf + HALO);
-    const integer nelems = end - start;
-
-    #pragma acc kernels copyin(sxxptr[start:nelems], syyptr[start:nelems], szzptr[start:nelems], syzptr[start:nelems], sxzptr[start:nelems], sxyptr[start:nelems]) \
-                        copyin(vxu[start:nelems], vxv[start:nelems], vxw[start:nelems])  \
-                        copyin(vyu[start:nelems], vyv[start:nelems], vyw[start:nelems])  \
-                        copyin(vzu[start:nelems], vzv[start:nelems], vzw[start:nelems])  \
-                        present(cc11[start:nelems], cc12[start:nelems], cc13[start:nelems], cc14[start:nelems], cc15[start:nelems], cc16[start:nelems])             \
-                        present(cc22[start:nelems], cc23[start:nelems], cc24[start:nelems], cc25[start:nelems], cc26[start:nelems])                   \
-                        present(cc33[start:nelems], cc34[start:nelems], cc35[start:nelems], cc36[start:nelems])                         \
-                        present(cc44[start:nelems], cc45[start:nelems], cc46[start:nelems])                               \
-                        present(cc55[start:nelems], cc56[start:nelems])                                     \
-                        present(cc66[start:nelems])                                           \
-                        async(phase) 
+    #pragma acc kernels
     #pragma acc loop independent
 #elif defined(_OPENMP)
     #pragma omp parallel for
@@ -1096,12 +1016,12 @@ void compute_component_scell_BL (s_t             s,
     for (integer y = ny0; y < nyf; y++)
     {
 #if defined(_OPENACC)
-        #pragma acc loop independent device_type(nvidia) gang worker(4)
+        #pragma acc loop independent
 #endif
         for (integer x = nx0; x < nxf; x++)
         {
 #if defined(_OPENACC)
-            #pragma acc loop independent device_type(nvidia) gang vector(32) 
+            #pragma acc loop independent
 #elif defined(__INTEL__COMPILER)
             #pragma simd
 #endif
