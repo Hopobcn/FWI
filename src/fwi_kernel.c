091aee8b4e2c975476e32217eb384026629c4709
Pau Farre <pau.farre@bsc.es>
2017-04-20 18:10:52 +0200

    Simplify array initialization

--- src/fwi_kernel.c
+++ src/fwi_kernel.c
@@ -38,8 +38,7 @@ void set_array_to_random_real( real* restrict array, const integer length)
 
     print_debug("Array is being initialized to %f", randvalue);
 
-    for( integer i = 0; i < length; i++ )
-        array[i] = randvalue;
+    set_array_to_constant(array, randvalue, length);
 }
 
 /*
